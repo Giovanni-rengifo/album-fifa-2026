bc71fb7206861076fd89f7ae1f702685be34e50c
Giovanni <giovanni.rengifo@gmail.com>
2026-05-25 17:17:36 -0500

    Primera versión

---
+++ sw.js.js
@@ -0,0 +1,30 @@
+const CACHE = 'road2026-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
